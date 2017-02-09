8275237ab75aedee40d7a979116ec87abb6fe656
Hans <devwingcom@gmail.com>
2017-02-09 21:57:28 +0800

    Added simple balloon when the committer is changed.

--- src/ChangeAction.java
+++ src/ChangeAction.java
@@ -1,5 +1,10 @@
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+
+import javax.swing.*;
 
 /**
  * Created by hans on 2017/2/9.
@@ -8,6 +13,17 @@ public class ChangeAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        Notification notification = new Notification(
+            "GitCommitterSwitch",
+            "Message",
+            "Changed the current git committer to \"" + this.getTemplatePresentation().getText() + "\"",
+            NotificationType.INFORMATION
+        );
+        Notifications.Bus.notify(notification);
+
+        Timer timer = new Timer(2000, e1 -> {
+            notification.expire();
+        });
+        timer.start();
     }
 }
