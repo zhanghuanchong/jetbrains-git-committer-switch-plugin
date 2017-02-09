e71616b5d8ddf7359e8e104321906d76b8483934
Hans <devwingcom@gmail.com>
2017-02-09 21:12:29 +0800

    Use QuickSwitchSchemaAction instead of simple popup.

---
+++ src/ChangeAction.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by hans on 2017/2/9.
+ */
+public class ChangeAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
---
+++ src/ShowPopupAction.java
@@ -0,0 +1,34 @@
+import com.intellij.ide.actions.QuickSwitchSchemeAction;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by hans on 2017/2/9.
+ */
+public class ShowPopupAction extends QuickSwitchSchemeAction {
+    protected ShowPopupAction() {
+        super();
+        this.getTemplatePresentation().setIcon(IconLoader.getIcon("/icons/users.png"));
+    }
+
+    @Override
+    protected String getPopupTitle(AnActionEvent e) {
+        return "Chang Git Committer";
+    }
+
+    @Override
+    protected void fillActions(Project project, @NotNull DefaultActionGroup defaultActionGroup, @NotNull DataContext dataContext) {
+        String[] users = {"User 1", "User 2"};
+        for (String u: users) {
+            AnAction action = new ChangeAction();
+            action.getTemplatePresentation().setText(u);
+
+            defaultActionGroup.addAction(action);
+        }
+    }
+}
